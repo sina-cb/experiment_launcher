24f6d30edbfcf43ba4511220bfb5ca4f54b2191c
Sina Solaimanpour <sina@uga.edu>
2016-05-06 13:18:04 -0400

    Minor update and added the run_all_experiments.sh file

--- shellscripts/run_in_loop.sh
+++ shellscripts/run_in_loop.sh
@@ -2,7 +2,7 @@
 
 run_count=0;
 
-while test "$run_count" -lt "100"; do
+while test "$run_count" -lt "50"; do
 	export RUN_NUMBER=$run_count
 	echo "=========Experiment run_count: "$run_count
 	/home/sina/indigo_workspace/src/experiment_launcher/shellscripts/launcher.sh
