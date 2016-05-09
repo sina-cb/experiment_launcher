c07f41f13c46022b7def460449497c4b10c9c9e5
Sina Solaimanpour <sina@uga.edu>
2016-05-08 22:25:06 -0400

    Fixed Shell Script - Again

--- shellscripts/run_in_loop.sh
+++ shellscripts/run_in_loop.sh
@@ -6,7 +6,7 @@ while [ $c -le 15 ]
 do
 	export RUN_NUMBER=$c
 	echo "=========Experiment run_count: "$c
-	#/home/sina/indigo_workspace/src/experiment_launcher/shellscripts/launcher.sh
+	/home/sina/indigo_workspace/src/experiment_launcher/shellscripts/launcher.sh
 	c=$((c+1))
 	sleep 20s
 	echo $c
